cca1c8076cdaacac481b5a8f3918a6d193f1e8f5
KoyamaSohei <koyamaso0309@gmail.com>
2021-03-28 23:02:30 +0900

    04

---
+++ 04_type.lua
@@ -0,0 +1,9 @@
+print(type("What is my type"))   --> string
+local t = 10
+ABC = nil
+
+print(type(5.8*t))
+print(type(true))
+print(type(print))
+print(type(nil))
+print(type(type(ABC)) == "string")
